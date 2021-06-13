c07f856daf7fc248243d8828eeb782885db143e8
shiory602 <suzushioufo@gmail.com>
2021-06-12 21:12:48 -0700

    add icon

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
-import React from 'react';
+// import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Context from '../context/Context';
+import React, { useState, useEffect } from "react";
 
 /*---------------
 | Ant design     |
@@ -27,6 +27,53 @@ import Footer from '../Footer';
 
 function Main() {
 
+
+  // Initialize state
+  const [currentWeather, setCurrentWeather] = useState([]);
+  const [oneCall, setOneCall] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const[city, setCity] = useState('Vancouver');
+  const[lon, setLon] = useState('-123.1193');
+  const[lat, setLat] = useState('49.2497');
+  const[dt, setDt] = useState('1623554321');
+
+  function changeCity(cityName, lon, lat, dt) {
+      setCity(cityName);
+      setLon(lon);
+      setLat(lat);
+      setDt(dt);
+  }
+
+// Fetch data
+  useEffect(() => {
+      /*--------------------------------------
+      Weather API reference:
+      https://openweathermap.org/api/one-call-api
+      ---------------------------------------*/
+      const api = {
+          key: "c05165900889e0c017b2ee9ad3a1a515",
+          baseurl: "https://api.openweathermap.org/data/2.5/"
+      }
+      setIsLoading(true);
+  
+      fetch(`${api.baseurl}weather?q=${city}&units=metric&appid=${api.key}`)
+      .then(res => res.json())
+      .then(data => setCurrentWeather(data))
+      .catch(error => console.log(`error ${error}`))
+
+      console.log(currentWeather);
+      
+      fetch(`${api.baseurl}onecall?lat=${lat}&lon=${lon}&date=${dt}&exclude=current&units=metric&appid=${api.key}`)
+      .then(res => res.json())
+      .then(data => setOneCall(data))
+      .catch(error => console.log(`error ${error}`))
+
+      console.log(oneCall);
+
+    }, [city])
+
+
   return (
     <BrowserRouter>
       <Wrap>
@@ -34,7 +81,7 @@ function Main() {
           {/* main */}
           <Route exact path='/'>
               <Header />
-              <StickToday><Today /></StickToday>
+              <StickToday><Today currentWeather={currentWeather} /></StickToday>
           </Route>
           {/* detail */}
           <Route path='/Detail'>
@@ -81,3 +128,5 @@ const Layout = styled.div`
   margin: 50px;
   margin-top: 0;
 `
+
+
